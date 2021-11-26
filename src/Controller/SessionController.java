233e8e40b75d04ed318f7c0d81caa91bd4bb8053
Arthur Camargo <arthur.camargo@stone.com.br>
2021-11-25 21:38:52 -0300

    adding session controller

---
+++ src/Controller/SessionController.java
@@ -0,0 +1,28 @@
+package Controller;
+
+import DAO.UserDAO;
+import Model.User;
+
+public class SessionController {
+    public User login(String user, String password) throws Exception{
+
+        try(UserDAO userDAO = new UserDAO()) {
+
+            User userToLogin = new User();
+            userToLogin.setUserName(user);
+
+            User loggedUser = userDAO.getUser(userToLogin);
+
+            if(loggedUser == null) {
+                throw new Exception(" User not found! ");
+            } else if(loggedUser.getPassword().equals(password)){
+                return loggedUser;
+            }else {
+                throw new Exception("Password wrong !");
+            }
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
+    }
+}
